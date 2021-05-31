d92032114a958ecec15c2f45c1abe1dfaa336988
liwangping <liwangping@mail.hypers.com>
2021-05-31 22:35:19 +0800

    feat(静态资源打包): 增加静态资源打包配置，增加静态资源的解析loader和增加相应的css前缀

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import _ from "lodash";
+import "./index.less";
+import iconPng from "./icon.png";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = iconPng;
+
+  element.appendChild(myIcon);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,45 @@ module.exports = {
     filename: "main.js", // 输出的文件名称
     path: path.resolve(__dirname, "dist"), // 输出的文件夹
   },
+  module: {
+    rules: [
+      {
+        test: /\.(css|less)$/i,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "less-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+    ],
+  },
 };
